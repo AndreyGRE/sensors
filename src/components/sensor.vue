4df43b0e7ce37830887252c51d856e003ee70df6
AndreyGRE <andrey.grenev@mail.ru>
2024-02-09 20:33:24 +0300

    btn

--- src/components/sensor.vue
+++ src/components/sensor.vue
@@ -11,7 +11,7 @@
         <div class="humidity">
           Влажность:{{ this.dataSensor.humidity ? this.dataSensor.humidity.toFixed(2) + "%" : "нет данных" }}
         </div>
-        <button @click="deleteSensor(index)">удалить</button>
+        <b-button @click="deleteSensor(index)">удалить</b-button>
       </div>
     </b-card>
 
@@ -25,47 +25,12 @@ export default {
     dataSensor: Sensor,
     selectedItem: Array,
   },
-  created() {
-    console.log(this.dataSensor) 
-  },
-
   methods: {
     showInfo(index) {
       this.$emit("showInfo", index);
     },
     deleteSensor(index) {
       this.$emit("delete", index);
-    },
-    startRandomUpdates() {
-      this.allSensor.forEach((sensor) => {
-        this.updateSensorData(sensor);
-      });
-    },
-    updateSensorData(sensor) {
-      setInterval(() => {
-        if (sensor.temperature !== "") {
-          sensor.temperature = this.getRandomValue(
-            sensor.temperature,
-            0.1,
-            -0.1
-          ).toFixed(2);
-        }
-        if (sensor.humidity !== "") {
-          sensor.humidity = this.getRandomValue(
-            sensor.humidity,
-            0.1,
-            -0.1
-          ).toFixed(2);
-        }
-      }, this.getRandomTime());
-    },
-    // случайное значение времени в пределах 5 секунд
-    getRandomTime() {
-      return Math.round(Math.random() * 3000) + 2000;
-    },
-    // случайное значение
-    getRandomValue(value, max, min) {
-      return Number(value) + Math.random() * (max - min) + min;
     },
   },
 };
